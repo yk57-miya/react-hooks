dc404df71561a507b588cebbe97c1667ffcde49a
miyashita-andfactory <miyashita_yuki@andfactory.co.jp>
2023-10-26 16:46:40 +0900

    勉強会実装

--- pages/page1.tsx
+++ pages/page1.tsx
@@ -23,6 +23,43 @@ const reducerFunc = (countState: number, action: string) => {
   }
 };
 
+// 素数判定
+// 1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47;
+const IsPrimeNumber = (number: number) => {
+  if (number === 1) return true;
+
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// FizzBuzz
+const multiple3and5 = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+
+  // もっと簡潔な書き方
+  // for (let i = 1; i <= 100; i++) {
+  //   let output = '';
+  //   if (i % 3 === 0) output += 'Fizz';
+  //   if (i % 5 === 0) output += 'Buzz';
+  //   console.log(output || i);
+  // }
+};
+multiple3and5();
+
 const Page1: NextPage = () => {
   const [count, dispatch] = useReducer(reducerFunc, initialState);
 
@@ -79,6 +116,10 @@ const Page1: NextPage = () => {
               </Button>
             </div>
           </div>
+        </div>
+
+        <div className="max-w-5xl my-40 mx-auto gap-10">
+          {IsPrimeNumber(4) ? '素数です' : '素数ではありません'}
         </div>
 
         <div className="hidden md:flex max-w-5xl mx-auto gap-10">
